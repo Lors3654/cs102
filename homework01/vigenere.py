f07958525157760f2216228e6b7349c57a597aae
Lors3654 <temberline490@gmail.com>
2020-10-10 02:18:27 +0300

    Реализована функция encrypt_vigenere()

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -10,7 +10,12 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    key_length = len(keyword)
+    key_as_int = [ord(i) for i in keyword]
+    plaintext_int = [ord(i) for i in plaintext]
+    for i in range(len(plaintext_int)):
+        value = (plaintext_int[i] + key_as_int[i % key_length]) % 26
+        ciphertext += chr(value + 65)
     return ciphertext
 
 
@@ -26,5 +31,10 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    key_length = len(keyword)
+    key_as_int = [ord(i) for i in keyword]
+    ciphertext_int = [ord(i) for i in ciphertext]
+    for i in range(len(ciphertext_int)):
+        value = (ciphertext_int[i] - key_as_int[i % key_length]) % 26
+        plaintext += chr(value + 65)
     return plaintext
