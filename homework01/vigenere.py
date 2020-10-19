5b1021fa43ba067905eed04afe7c5c21caaf2198
Lors3654 <temberline490@gmail.com>
2020-10-19 03:43:19 +0300

    Fixed the Vigenere chipher

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,37 +1,32 @@
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     """
     Encrypts plaintext using a Vigenere cipher.
-
     >>> encrypt_vigenere("PYTHON", "A")
     'PYTHON'
     >>> encrypt_vigenere("python", "a")
     'python'
     >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
     'LXFOPVEFRNHR'
-    """
+    """     
     ciphertext = ""
-    for i in range(len(plaintext)):
-        x = plaintext[i]
-        y = keyword[i % len(keyword)]
-        if x.isalpha():
-            if 'A' <= y <= 'Z':
-                code = ord(x) + ord(y) - ord("A")
-                if (code > ord('Z')):
-                     code = code - 26
-            elif 'a' <= y <= 'z':
-                code = ord(x) + ord(y) - ord("a")
-                if (code > ord('z')):
-                    code = code - 26
-        else:
-            code=ord(x)
-        ciphertext += chr(code)
+    for i in range (len(plaintext)):
+        x = i % len(keyword)
+        y = keyword[x]
+        z = plaintext[i]
+        if 'A' <= z <= 'Z':
+            key = ord(y) - ord("A")
+            code = chr((ord(z) - ord("A") + key) % 26 + ord("A"))
+        elif 'a' <= z <= 'z':
+            key = ord(y) - ord("a")
+            code = chr((ord(z) - ord("a") + key) % 26 + ord("a"))
+        else: code = z
+        ciphertext = ciphertext + code
     return ciphertext
 
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     """
     Decrypts a ciphertext using a Vigenere cipher.
-
     >>> decrypt_vigenere("PYTHON", "A")
     'PYTHON'
     >>> decrypt_vigenere("python", "a")
@@ -39,21 +34,17 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
     'ATTACKATDAWN'
     """
-    plaintext = ""
-    
-    for i in range(len(ciphertext)):
-        x = ciphertext[i]
-        y = keyword[i % len(keyword)]
-        if x.isalpha():
-            if 'A' <= y <= 'Z':
-                code = ord(x) - ord(y) + ord("A")
-                if (code < ord('A')):
-                    code = code + 26
-            elif 'a'<= y <= 'z':
-                code = ord(x) - ord(y) + ord("a")
-                if (code < ord('a')):
-                    code = code + 26
-        else:
-            code=ord(x) 
-        plaintext += chr(code)
+    plaintext = ''
+    for i in range (len(ciphertext)):
+        x = i % len(keyword)
+        y = keyword[x]
+        z = ciphertext[i]
+        if 'A' <= z <= 'Z':
+            key = ord(y) - ord("A")
+            code = chr((ord(z) - ord("A") - key) % 26 + ord("A"))
+        elif 'a' <= z <= 'z':
+            key = ord(y) - ord("a")
+            code = chr((ord(z) - ord("a") - key) % 26 + ord("a"))
+        else: code = z
+        plaintext = plaintext + code
     return plaintext
