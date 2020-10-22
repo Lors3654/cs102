543617f97355e03fa8acbe36c6c857d46be35bc2
Lors3654 <temberline490@gmail.com>
2020-10-22 23:31:46 +0300

    Fixed the Caesar cipher

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,5 +1,6 @@
 import typing as tp
 
+
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     """
     Encrypts plaintext using a Caesar cipher.
@@ -16,15 +17,16 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ciphertext = ""
     new_word = []
     for i in plaintext:
-        if ord("A")<= i <= ord("Z"):
+        if ord("A") <= ord(i) <= ord("Z"):
             ciphertext = chr((((ord(i) - 65) + shift) % 26) + 65)
             new_word.append(ciphertext)
-        elif ord("a")<= i <= ord("z"):
-            ciphertext = chr((((ord(i)- 97) + shift) % 26) + 97)
+        elif ord("a") <= ord(i) <= ord("z"):
+            ciphertext = chr((((ord(i) - 97) + shift) % 26) + 97)
             new_word.append(ciphertext)
-        else: 
+        else:
             new_word.append(i)
-    return ''.join(new_word)
+    return "".join(new_word)
+
 
 def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     """
@@ -42,12 +44,12 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     plaintext = ""
     new_word = []
     for i in ciphertext:
-        if ord("A")<= i <= ord("Z"):
+        if ord("A") <= ord(i) <= ord("Z"):
             plaintext = chr((((ord(i) - 65) - shift) % 26) + 65)
             new_word.append(plaintext)
-        elif ord("a")<= i <= ord("z"):
+        elif ord("a") <= ord(i) <= ord("z"):
             plaintext = chr((((ord(i) - 97) - shift) % 26) + 97)
             new_word.append(plaintext)
-        else: 
+        else:
             new_word.append(i)
-    return ''.join(new_word)
+    return "".join(new_word)
