3f71fd0df31c29ef95f0c5c36d4c43f109082ddd
Lors3654 <temberline490@gmail.com>
2021-01-19 16:05:03 +0300

    d

--- homework05/vkapi/config.py
+++ homework05/vkapi/config.py
@@ -2,6 +2,6 @@
 
 VK_CONFIG = {
     "domain": "https://api.vk.com/method",
-    "access_token": "9ddc9d6dba6e84e522d47b96db44ec440832cd195fc98b932a0da97c7e1f6601c806bce9d43da065b1a75",
+    "access_token": "",
     "version": "5.126",
 }
