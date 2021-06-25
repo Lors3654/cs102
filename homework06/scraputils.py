28d6884a073f6df3b20e091529090f8f7d4ad880
Lors3654 <temberline490@gmail.com>
2021-06-25 13:53:32 +0300

    scraputils

--- homework06/scraputils.py
+++ homework06/scraputils.py
@@ -1,19 +1,38 @@
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup  # type: ignore
 
 
 def extract_news(parser):
     """ Extract news from a given web page """
     news_list = []
 
-    # PUT YOUR CODE HERE
+    headlines = parser.table.findAll("tr", {"class": "athing"})
+    info = parser.table.findAll("td", {"class": "subtext"})
+
+    for i in range(len(headlines)):
+        comments = "0"
+        title = headlines[i].find(class_="storylink")
+        points = info[i].span.text.split()[0]
+        url = info[i].findAll("a")
+        author = url[0].text
+        if url[-1].text != "discuss":
+            comments = url[-1].text.split()[0]
+        news = {
+            "author": author,
+            "comments": comments,
+            "points": points,
+            "title": title.text,
+            "url": title["href"],
+        }
+        news_list.append(news)
 
     return news_list
 
 
 def extract_next_page(parser):
     """ Extract next page URL """
-    # PUT YOUR CODE HERE
+    next = parser.find("a", {"class": "morelink"})
+    return next["href"]
 
 
 def get_news(url, n_pages=1):
@@ -29,4 +48,3 @@ def get_news(url, n_pages=1):
         news.extend(news_list)
         n_pages -= 1
     return news
-
