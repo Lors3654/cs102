a68fdfbde27600c646242306bffafd7676c6255d
Lors3654 <temberline490@gmail.com>
2021-06-26 16:00:36 +0300

    all

--- homework07-web/static_server/static_server.py
+++ homework07-web/static_server/static_server.py
@@ -6,8 +6,13 @@ import typing as tp
 from datetime import datetime
 from urllib.parse import quote, unquote, urlparse, urlunparse
 
-from httpserver import (BaseHTTPRequestHandler, BaseRequestHandler,
-                        HTTPRequest, HTTPResponse, HTTPServer)
+from httpserver import (
+    BaseHTTPRequestHandler,
+    BaseRequestHandler,
+    HTTPRequest,
+    HTTPResponse,
+    HTTPServer,
+)
 
 
 def url_normalize(path: str) -> str:
